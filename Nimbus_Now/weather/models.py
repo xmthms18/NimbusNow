7ec50757f383656d50dba862ee8d7f47706b27b7
xmthms18 <xmthms@gmail.com>
2023-08-24 14:03:37 -0400

    added model for saved weather data

--- Nimbus_Now/weather/models.py
+++ Nimbus_Now/weather/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class SavedWeatherData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=200)
+    icon = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.city}"
