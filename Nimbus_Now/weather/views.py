b241dab16e155e26fac1564aef4279f880f2c977
xmthms18 <xmthms@gmail.com>
2023-08-24 10:39:14 -0400

    edited urls and added css file

--- Nimbus_Now/weather/views.py
+++ Nimbus_Now/weather/views.py
@@ -6,21 +6,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    API_KEY = open("API_KEY", "r").read()
+    API_KEY = "ea176ef47b541be3c93f392589821086"
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
     if request.method == 'POST':
         city1 = request.POST['city1']
-        city2 = request.GET('city2', None)
+        city2 = request.POST.get('city2', None)
 
         weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
 
         if city2:
             weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url,forecast_url)
-
         else:
-
             weather_data2, daily_forecasts2 = None, None
 
         context = {
@@ -31,17 +29,42 @@ def index(request):
         }
 
         return render(request, 'weather_app/index.html', context)
+    
     else:
         return render(request, 'weather_app/index.html')
     
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
-      response = requests.get(current_weather_url.format(city, api_key)).json()
-      lat, lon = response['coord']['lat'], response['coord']['lon']
-      forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+      response = requests.get(current_weather_url.format(city, api_key )).json()
+      print("API response: ", response) # debugging
+
+      if 'coord' in response:
+          lat, lon = response['coord']['lat'], response['coord']['lon']
+      else:
+          print('The key "coord" was not found in the response')
+          lat, lon = None, None
+
+      if lat and lon:
+          with requests.get(forecast_url.format(lat, lon, api_key)) as forecast_response:
+              forecast_data = forecast_response.json()
+      else:
+          forecast_data = None
 
       weather_data = {
         'city': city,
         'temperature': round(response['main']['temp'] - 273.15, 2),
         'description': response['weather'][0]['description'],
         'icon': response['weather'][0]['icon'],
-    }
+      }
+      
+      daily_forecasts = []
+      if forecast_data and 'daily' in forecast_data:
+          for daily_data in forecast_data['daily'][:5]:
+              daily_forecasts.append({
+                  'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+                  'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+                  'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+                  'description': daily_data['weather'][0]['description'],
+                  'icon': daily_data['weather'][0]['icon'],
+              })
+
+      return weather_data, daily_forecasts
