a85db5774b5e2f156efdf664d1e858917f43deac
xmthms18 <xmthms@gmail.com>
2023-08-24 14:08:12 -0400

    added url for save_weather_data

--- Nimbus_Now/weather/urls.py
+++ Nimbus_Now/weather/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name= "index")
+    path("",views.index, name= "index"),
+    path('save_weather_data/', views.save_weather_data, name='save_weather_data'),
+
+    
 ]
