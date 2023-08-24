906bf11db6488e51e58a362c0dd0f20ef1dfefb5
xmthms18 <xmthms@gmail.com>
2023-08-24 08:53:42 -0400

    apikey request

--- Nimbus_Now/weather/views.py
+++ Nimbus_Now/weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
+import datetime
 
 # Create your views here.
+
+def index(request):
+    API_KEY = open("API_KEY", "r").read()
+    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
+    
