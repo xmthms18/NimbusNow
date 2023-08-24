ee3716ce4b95f1f1d41467fe796bdd48ecf30c47
xmthms18 <xmthms@gmail.com>
2023-08-24 15:32:24 -0400

    added login url

--- Nimbus_Now/Nimbus_Now/settings.py
+++ Nimbus_Now/Nimbus_Now/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+LOGIN_URL = 'login' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
