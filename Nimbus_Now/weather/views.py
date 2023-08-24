2ae5b2e97000261f9b298cc6b798002b149b8eb0
xmthms18 <xmthms@gmail.com>
2023-08-24 11:30:12 -0400

    changed temp to f

--- Nimbus_Now/weather/views.py
+++ Nimbus_Now/weather/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import render
 
 def index(request):
     API_KEY = "ea176ef47b541be3c93f392589821086"
-    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
-    forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
+    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=imperial'
+    forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}&units=imperial'
 
     if request.method == 'POST':
         city1 = request.POST['city1']
@@ -51,7 +51,7 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
 
       weather_data = {
         'city': city,
-        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'temperature': round(response['main']['temp'], 2),
         'description': response['weather'][0]['description'],
         'icon': response['weather'][0]['icon'],
       }
@@ -61,8 +61,8 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
           for daily_data in forecast_data['daily'][:5]:
               daily_forecasts.append({
                   'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
-                  'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
-                  'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+                  'min_temp': round(daily_data['temp']['min'], 2),
+                  'max_temp': round(daily_data['temp']['max'], 2),
                   'description': daily_data['weather'][0]['description'],
                   'icon': daily_data['weather'][0]['icon'],
               })
