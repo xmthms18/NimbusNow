f24f785cdff8bc9283f0ed0a6cbed93458d0ce1b
xmthms18 <xmthms@gmail.com>
2023-08-24 15:32:01 -0400

    updated urls

--- Nimbus_Now/Nimbus_Now/urls.py
+++ Nimbus_Now/Nimbus_Now/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-#from weather.views import signup_view  # Make sure the correct path is used for your views
+from weather.views import signup_view  # Make sure the correct path is used for your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls')),  # Assuming 'weather' is your app's URL configuration
 
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    #path('signup/', signup_view, name='signup'),  # Use the correct import path for your views
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', signup_view, name='signup'),  # Use the correct import path for your views
 ]
